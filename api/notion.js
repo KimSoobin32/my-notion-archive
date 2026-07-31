e72f914d8dbbeddc8f4216d28baa4dfaae5f0752
KimSoobin32 <76720785+KimSoobin32@users.noreply.github.com>
2026-07-31 14:34:43 +0900

    Update notion.js

--- api/notion.js
+++ api/notion.js
@@ -1,7 +1,7 @@
 import { Client } from '@notionhq/client';
 
-const notion = new Client({ auth: process.env.ntn_G8886361609aransf6bOgNDhx6RS40jGhBY6Z53hmeUguc });
-const DATABASE_ID = process.env.1b4211ab1e09800e8c64d3d107c2c49a;
+const notion = new Client({ auth: process.env.NOTION_KEY });
+const DATABASE_ID = process.env.NOTION_DATABASE_ID;
 
 export default async function handler(req, res) {
   // 환경변수 누락 체크
